7c5f893f9e970b7e4b6e11ab77970b31cbc4df09
J. Pablo Pérez Trabucco <jpereztrabucco@gmail.com>
2014-05-06 17:47:49 -0400

    updates scraper.rb

--- scraper.rb
+++ scraper.rb
@@ -4,28 +4,45 @@
 require 'scraperwiki'
 require 'mechanize'
 
-agent = Mechanize.new
+class Diputados
 
-url = "http://www.congreso.gob.gt/legislaturas.php"
+  # For each parliamentarian
+  def per_person profile_url, uid
+    agent = Mechanize.new
+    profile_page = agent.get(profile_url)
+    profile = profile_page.at('#datos_contacto #votos').search('li')
 
+    record = {
+      "uid" => uid,
+      "name" => (profile[0].inner_text).gsub('Nombre:','').squeeze(' ').strip,
+      "email" => '', #(profile[1].inner_text).gsub('E-mail:','').squeeze(' ').strip, #Can't scrape this value, is protected by Cloudflare http://www.cloudflare.com/email-protection
+      "phone" => (profile[2].inner_text).gsub('Teléfono de Oficina:','').strip,
+      "address" => (profile[3].inner_text).gsub('Dirección de Oficina:','').strip,
+      "url" => profile_url
+    }
 
-# Read in a page
-page = agent.get(url)
+    puts '<---------------'
+    puts record
+    puts '--------------/>'
+    # ScraperWiki.save_sqlite(["uid"], record)
+  end
 
-page.search('.dir_tabla tr').drop(1).each do |li|
-  record = {
-    "name" => li.search('td')[1].inner_text.squeeze(' ').strip,
-    "url" => (li.search('td')[1]).at('a')["href"],
-    # "member_for" => li.search('dd')[0].inner_text,
-    # "party" => li.search('dd')[1].inner_text
-  }
-  puts '<---------------'
-  puts record
-  puts '--------------/>'
-  # exit
-  # ScraperWiki.save_sqlite(["name"], record)
+  # Obtains the profiles
+  def process
+    agent = Mechanize.new
+    url = "http://www.congreso.gob.gt/legislaturas.php"
+
+    # Read in a page
+    page = agent.get(url)
+
+    page.search('.dir_tabla tr').drop(1).each_with_index do |li, i|
+      url = (li.search('td')[1]).at('a')["href"]
+      per_person url, i+1
+      exit # for fast testing, only the first parliamentarian
+    end
+  end
 end
-#
+
 # # Find somehing on the page using css selectors
 # p page.at('div.content')
 #
@@ -38,3 +55,8 @@ end
 # on Morph for Ruby (https://github.com/openaustralia/morph-docker-ruby/blob/master/Gemfile) and all that matters
 # is that your final data is written to an Sqlite database called data.sqlite in the current working directory which
 # has at least a table called data.
+
+# Runner
+if !(defined? Test::Unit::TestCase)
+  Diputados.new.process
+end
